8c4b1f7cbd3e78e5dc15b7b2778b4783b38f4111
Mark Brough <mark.brough@gmail.com>
2019-04-22 11:36:22 +0000

    Initial commit

---
+++ scraper.py
@@ -0,0 +1,38 @@
+
+import exchangerates.get_rates as gr
+from io import StringIO
+environ['SCRAPERWIKI_DATABASE_NAME'] = 'sqlite:///data.sqlite'
+import scraperwiki
+import csv
+from datetime import datetime
+
+key = ['Date', "Currency", "Frequency", "Source"]
+
+
+def save_status(started_at, finished_at=None, success=False):
+    scraperwiki.sqlite.save(
+        ['started_at'],
+        {
+            'started_at': started_at,
+            'finished_at': finished_at,
+            'success': success,
+        },
+        'status'
+    )
+
+scrape_started_at = datetime.now().isoformat()
+save_status(scrape_started_at)
+
+def run_scraper():
+	gr.update_rates("consolidated.csv")
+	the_file = open("consolidated.csv", "r")
+	the_csv = csv.DictReader(the_file)
+	for row in the_csv:
+		row["RateFirstSeen"] = datetime.now().isoformat()
+		row["Date"] = datetime.strptime(row["Date"], "%Y-%m-%d")
+		scraperwiki.sqlite.save(key, row, 'rates')
+
+run_scraper()
+
+scrape_finished_at = datetime.now().isoformat()
+save_status(scrape_started_at, scrape_finished_at, True)
